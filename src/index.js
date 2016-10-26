df7c6d9c0f6e16868847e7f85e7661f10027c7dd
Eric Caleb <calebme@mail.com>
2016-10-26 14:07:58 +0100

    Added chooser component to router

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { render } from 'react-dom';
 import Main from './components/App';
 import Inbox from './components/Inbox';
 import Contact from './components/Contact';
+import Choose from './components/Choose';
 import Login from './components/Login';
 import PostThread from './components/PostThread';
 import Reports from './components/Reports';
@@ -19,6 +20,7 @@ const router = (
       <Route path="/" component={Main}>
         {/*<IndexRoute component={Login}></IndexRoute>*/}
         <IndexRoute component={Login}></IndexRoute>
+        <Route path="/choose" component={Choose}/>
         <Route path="/inbox" component={Inbox}></Route>
         <Route path="/contacts/:contactId" component={Contact} />
         <Route path="/posts/:postId" component={PostThread} />
